1658e8427b254662f8971b0f8612a1b06aebf5e1
johnhw <johnhw@gmail.com>
2016-06-17 10:53:49 +0100

    Added config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+# depth thresholds (in mm)
+max_depth = 2000
+min_depth = 300
+# minimum area for holes/outlines (in pixels)
+outer_contour_min_area = 6000
+hole_contour_min_area = 1200
+external_contour_min_area = 100
+# closing filter size
+closing_size = 8
+
+# gesture switching time, in seconds
+gesture_switch_time = 56
+
+
+# time before forced gesture switch, in pilot
+pilot_max_time = 6.0
+
+# number of gestures in pilot
+pilot_gestures = 80
